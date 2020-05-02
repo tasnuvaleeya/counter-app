92ab31651034a974a9884113d9dc13769dcb7f91
tasnuvaleeya <tasnuva2606@gmail.com>
2020-05-02 13:59:07 +0600

    started with new component

---
+++ src/components/counter.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+    state = {
+        count:0
+    }
+  render() {
+    return (
+      <div>
+        <span>{this.formatCount()}</span>
+        <button>Increment</button>
+      </div>
+    );
+  }
+
+  formatCount () {
+      const { count } = this.state;
+      return count === 0 ? "Zero" : count;
+    //   return count === 0 ? <h1>Zero</h1> : count;
+  }
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counter';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +17,5 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
