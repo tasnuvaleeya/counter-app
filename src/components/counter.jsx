28289de98fb7bcfc7ca93912bebf3d0a481b968b
tasnuvaleeya <tasnuva2606@gmail.com>
2020-05-02 15:28:16 +0600

    update state: setState

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,6 +7,8 @@ class Counter extends Component {
 
   handleIncrement = () => {
     console.log("Increment Clicked!", this);
+
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
