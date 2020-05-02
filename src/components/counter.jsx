d47e448f837c4e6dc94bbfc8ae6cdbd5a22ee998
tasnuvaleeya <tasnuva2606@gmail.com>
2020-05-02 14:42:41 +0600

    rendering list, understanding key

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,32 +1,36 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {
-        count:0
-    }
-    styles = {
-        fontSize:30,
-        fontWeight: "bold",
-    }
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+  styles = {
+    fontSize: 30,
+    fontWeight: "bold",
+  };
 
   render() {
     return (
       <div>
-        <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <span style={this.styles} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+    <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
       </div>
     );
   }
 
   getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-        return classes;
-    }
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 
-  formatCount () {
-      const { count } = this.state;
-      return count === 0 ? "Zero" : count;
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
