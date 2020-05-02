7519106e214264a1e49cec3cae9b635a46b30a5e
tasnuvaleeya <tasnuva2606@gmail.com>
2020-05-02 14:36:12 +0600

    refactoring

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,14 +8,23 @@ class Counter extends Component {
         fontSize:30,
         fontWeight: "bold",
     }
+
   render() {
+      let classes = this.getBadgeClasses();
+
     return (
       <div>
-        <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
+
+  getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
 
   formatCount () {
       const { count } = this.state;
