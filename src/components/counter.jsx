44bae38f8f09a0da9fc0f50a3f59c3c14a614b37
tasnuvaleeya <tasnuva2606@gmail.com>
2020-05-02 17:02:25 +0600

    composing components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -9,28 +9,24 @@ class Counter extends Component {
        fontWeight: "bold",   
   }
 
-  handleIncrement = (product) => {
+  handleIncrement = product => {
     console.log(product);
-
-    // console.log("Increment Clicked!", this);
-
     this.setState({ count: this.state.count + 1 });
   };
 
   render() {
     return (
       <div>
-        <span style={this.styles} className={this.getBadgeClasses()}>
+        <span style={this.styles} 
+        className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         <button
-          onClick={() => this.handleIncrement(product)}
+          onClick={ (product) => this.handleIncrement(product) }
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-        {/* {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.renderTags()} */}
       </div>
     );
   }
