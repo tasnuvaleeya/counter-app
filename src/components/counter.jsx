73ad17653de241ce94682b6601285ce3c9e1308b
tasnuvaleeya <tasnuva2606@gmail.com>
2020-05-02 14:28:56 +0600

    styling element

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,11 +4,15 @@ class Counter extends Component {
     state = {
         count:0
     }
+    styles = {
+        fontSize:30,
+        fontWeight: "bold",
+    }
   render() {
     return (
       <div>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
